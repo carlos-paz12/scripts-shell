11baece4b3965ca217b26a201a74d69c099d40d8
carlos-paz12 <pzzz.silva@gmail.com>
2024-01-02 11:49:54 -0300

    Add INTERFACES explain

---
+++ interface.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+# dialog --msgbox "Lorem ipsum sit dolor amet consectur..." 10 30
+# 'msgbox'  exibe uma janela com uma mensagem e um botão de ação
+
+# dialog --infobox "Lorem ipsum sit dolor amet consectur..." 10 30
+# 'infobox' exibe uma janela com uma mensagem
+
+# dialog --textbox ./interval.sh 10 30
+# 'textbox' exibe uma janela com uma mensagem com base em um arquivo
+
+# dialog --yesno "Lorem ipsum sit dolor amet consectur..." 10 30
+# 'yesno' exibe uma janela com opções de 'sim' e 'não'
+
+# dialog --gauge "Lorem ipsum sit dolor amet consectur..." 10 30
+# 'gauge' exibe uma janela com barra de progresso
+
+# dialog --checklist "Lorem ipsum sit dolor amet consectur..." 10 30
+# 'checklist' exibe uma janela com menu de múltipla escolha
+
+# dialog --menu "Lorem ipsum sit dolor amet consectur..." 10 30
+# 'menu' exibe uma janela com menu de escolha única
+
+# dialog --calendar "Lorem ipsum sit dolor amet consectur..." 10 30
+# 'calendar' exibe uma janela com um calendário
+
+# dialog --fselect "Lorem ipsum sit dolor amet consectur..." 10 30
+# 'fselect' exibe uma janela para seleção de arquivos
+
+# dialog --passwordbox "Lorem ipsum sit dolor amet consectur..." 10 30
+# 'passwordbox' exibe uma janela com caixa de entrada para senha
