bb7809b267a329da98ef2a506bc8fb1ddaac99ec
carlos-paz12 <pzzz.silva@gmail.com>
2024-01-08 10:13:41 -0300

    Add way to capture user response in dialog yesno

--- interface.sh
+++ interface.sh
@@ -6,11 +6,22 @@
 # dialog --infobox "Lorem ipsum sit dolor amet consectur..." 10 30
 # 'infobox' exibe uma janela com uma mensagem
 
-# dialog --textbox ./interval.sh 10 30
+# dialog --textbox ./README.md 10 30
 # 'textbox' exibe uma janela com uma mensagem com base em um arquivo
 
-# dialog --yesno "Lorem ipsum sit dolor amet consectur..." 10 30
+dialog --yesno "Deseja ver o contrato de licença?" 10 30
 # 'yesno' exibe uma janela com opções de 'sim' e 'não'
+
+# Way to capture the user response
+res=$?
+echo $res
+if [ $res -eq 0 ];
+then
+    dialog --textbox ./README.md 10 30
+else
+    clear
+    echo "Bye!"
+fi
 
 # dialog --gauge "Lorem ipsum sit dolor amet consectur..." 10 30
 # 'gauge' exibe uma janela com barra de progresso
